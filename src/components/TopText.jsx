c0f3122a89c7b7471501485d7bd02433885dbdd4
Ian <69704125+ian-cunha@users.noreply.github.com>
2023-11-30 04:05:26 -0300

    update

--- src/components/TopText.jsx
+++ src/components/TopText.jsx
@@ -2,7 +2,7 @@ function TopText() {
   return (
     <div className="topText">
       <h1 className="firstText text-primary">EDF. Morada da Torre</h1>
-      <p className="secondText">Localizado a 100 metros da BEIRA-RIO. Um empreendimento com excelente acabamento e ótima localização.</p>
+      <p className="secondText">More a 100 metros da Avenida Beira Rio. Um empreendimento com excelente acabamento e ótima localização.</p>
       <h2 className="blockText">APARTAMENTO PRONTO PARA MORAR!</h2>
     </div>
   )
