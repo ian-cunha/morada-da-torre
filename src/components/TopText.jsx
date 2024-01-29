3db602d0a7458311edb91b4d3073c345746b030a
Ian <69704125+ian-cunha@users.noreply.github.com>
2024-01-29 16:30:23 -0300

    update 29/01/24
    Adicionado valor do imóvel no topo do site.

--- src/components/TopText.jsx
+++ src/components/TopText.jsx
@@ -4,6 +4,8 @@ function TopText() {
       <h1 className="firstText text-primary">Edf. Morada da Torre</h1>
       <p className="secondText">More a 100 metros da Avenida Beira Rio. Um empreendimento com excelente acabamento e ótima localização.</p>
       <h2 className="blockText">APARTAMENTO PRONTO PARA MORAR!</h2>
+      <h2 className="textValue">A partir de:</h2>
+      <h2 className="valueTop">R$360.000</h2>
     </div>
   )
 }
